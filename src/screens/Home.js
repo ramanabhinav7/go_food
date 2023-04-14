f6e86ee6c6b291058dc632f4b51b833cfeed5fe5
Amish Kumar Singh <amish.kumar1211@gmail.com>
2023-04-14 23:40:21 +0530

    setup database and basic frontend

---
+++ src/screens/Home.js
@@ -0,0 +1,21 @@
+import React from "react";
+import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
+import Card from "../components/Card";
+import Carousel from "../components/Carousel";
+
+export default function Home() {
+  return (
+    <>
+      <Navbar />
+      <Carousel />
+      <div className="m-3">
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+      </div>
+      <Footer />
+    </>
+  );
+}
