b6d6de941c8e91d10308fbdac59e8df726f6efb4
ramanabhinav7 <abhinav.90444@gmail.com>
2023-04-18 01:12:07 +0530

    search, implemented and previour bug solved by amish

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,34 +1,41 @@
-import React from "react";
+import React from "react"
 
 export default function Card(props) {
+  console.log("I'm card")
+
   let options = props.options;
+  console.log("I'm option",options)
   let priceOptions = Object.keys(options);
+  console.log("I'm priceOptions")
+  
+
   return (
     <div className="card mt-3" style={{ width: "18rem", maxHeight: "360px" }}>
       <img
-        src="https://media.istockphoto.com/photos/chilli-paneer-tikka-or-paneer-kabab-picture-id629423010?k=20&m=629423010&s=612x612&w=0&h=X-OBIncHWjNFqxGfQuQHwrYHi2GcUw-jn7kqqZpHSw8="
+        src={props.imgSrc}
         className="card-img-top"
         alt="..."
       />
       <div className="card-body">
         <h5 className="card-title">{props.foodName}</h5>
-        <p className="card-text">This is some important text.</p>
         <div className="container w-100">
           <select className="m-2 h-100  bg-success rounded">
-            {new Array(5).fill(0).map((_, index) => (
-              <option key={index + 1} value={index + 1}>
+            { Array.from(Array(6),(e,index)=>{
+                return(
+                  <option key={index + 1} value={index + 1}>
                 {index + 1}
               </option>
-            ))}
+                )
+            })}
           </select>
 
           <select className="m-2 h-100  bg-success rounded">
             {priceOptions.map((data) => {
+             
               return (
-                <option key={data} value={data}>
-                  {data}
-                </option>
-              );
+                
+                <option key={data} value={data}>{data}</option>
+              )
             })}
           </select>
 
--- src/screens/Home.js
+++ src/screens/Home.js
@@ -2,22 +2,20 @@ import React, { useEffect, useState } from "react";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
 import Card from "../components/Card";
-import Carousel from "../components/Carousel";
-
+// import Carousel from "../components/Carousel";
 
 export default function Home() {
-
-  const [foodCat, setFoodCat] = useState([]);  // agar dot map function use karna hai to [ ] array hona chahiye aur agar object ke andar loop karana hai to for : in loop use kiya jayega
+  const [search, setSearch] = useState("");
+  const [foodCat, setFoodCat] = useState([]); // agar dot map function use karna hai to [ ] array hona chahiye aur agar object ke andar loop karana hai to for : in loop use kiya jayega
   const [foodItem, setFoodItem] = useState([]);
 
   const loadData = async () => {
     let response = await fetch("http://localhost:5000/api/foodData", {
       method: "POST",
       headers: {
-        'Content-Type': 'application/json'
-      }
+        "Content-Type": "application/json",
+      },
     });
-
 
     response = await response.json();
     // console.log("debug: response ",response)
@@ -25,58 +23,140 @@ export default function Home() {
 
     setFoodItem(response[0]);
     setFoodCat(response[1]);
-
-  }
+  };
 
   useEffect(() => {
-    loadData()
-  }, [])
-
-
-
-
+    loadData();
+  }, []);
 
   return (
     <div>
-      <div><Navbar /></div>
-      <div><Carousel /></div>
+      <div>
+        <Navbar />
+      </div>
+      <div>
+        <div
+          id="carouselExampleFade"
+          className="carousel slide carousel-fade"
+          data-bs-ride="carousel"
+          style={{ objectFit: "contain !important" }}
+        >
+          <div className="carousel-inner" id="carousel">
+            <div className="carousel-caption" style={{ zIndex: "10" }}>
+              <div className="d-flex justify-content-center">
+                <input
+                  className="form-control me-2"
+                  type="search"
+                  placeholder="Search"
+                  aria-label="Search"
+                  value={search}
+                  onChange={(e) => {
+                    setSearch(e.target.value);
+                  }}
+                />
+                {/* <button
+                  className="btn btn-outline-success text-white bg-success"
+                  type="submit"
+                >
+                  Search
+                </button> */}
+              </div>
+            </div>
+
+            <div className="carousel-item active">
+              <img
+                src="https://source.unsplash.com/random/900x700/?burger"
+                className="d-block w-100"
+                alt="..."
+                style={{ filter: "brightness(50%)" }}
+              />
+            </div>
+            <div className="carousel-item">
+              <img
+                src="https://source.unsplash.com/random/900x700/?pastry"
+                className="d-block w-100"
+                alt="..."
+                style={{ filter: "brightness(50%)" }}
+              />
+            </div>
+            <div className="carousel-item">
+              <img
+                src="https://source.unsplash.com/random/900x700/?barbeque"
+                className="d-block w-100"
+                alt="..."
+                style={{ filter: "brightness(50%)" }}
+              />
+            </div>
+          </div>
+          <button
+            className="carousel-control-prev"
+            type="button"
+            data-bs-target="#carouselExampleFade"
+            data-bs-slide="prev"
+          >
+            <span
+              className="carousel-control-prev-icon"
+              aria-hidden="true"
+            ></span>
+            <span className="visually-hidden">Previous</span>
+          </button>
+          <button
+            className="carousel-control-next"
+            type="button"
+            data-bs-target="#carouselExampleFade"
+            data-bs-slide="next"
+          >
+            <span
+              className="carousel-control-next-icon"
+              aria-hidden="true"
+            ></span>
+            <span className="visually-hidden">Next</span>
+          </button>
+        </div>
+      </div>
 
       <div className="container">
-        {
-          foodCat.length !== 0
-            ? foodCat.map((data) => {
-              return (<div className="row mb-3">
-                  <div key={data._id} className="fs-3 m-3">
-                    {data.CategoryName}
-                  </div>
-                  <hr />
-                  {foodItem !== [] ?
-
-                    foodItem.filter((item) => item.CategoryName === data.CategoryName)
-                      .map(filterItems => {
+        {foodCat.length !== 0 ? (
+          foodCat.map((data) => {
+            return (
+              <div className="row mb-3">
+                <div key={data._id} className="fs-3 m-3">
+                  {data.CategoryName}
+                </div>
+                <hr />
+                {foodItem !== []
+                  ? foodItem
+                      .filter(
+                        (item) =>
+                          (item.CategoryName === data.CategoryName) &&
+                          (item.name.toLowerCase().includes(search.toLocaleLowerCase()))
+                      )
+                      .map((filterItems) => {
                         return (
-                          <div key={filterItems._id} className="col-12 col-md-6 col-lg-3">
-                            <Card foodName ={ filterItems.name}
-                            options = {filterItems.options}
-                            imgSrc={filterItems.img}
+                          <div
+                            key={filterItems._id}
+                            className="col-12 col-md-6 col-lg-3"
+                          >
+                            <Card
+                              foodName={filterItems.name}
+                              options={filterItems.options[0]}
+                              imgSrc={filterItems.img}
                             ></Card>
                           </div>
-                        )
+                        );
                       })
-                    : "No Such Data"}
-
-                </div>
-
-              )
-            })
-            : <div>"""""""""""""""""""""""</div>
-
-        }
-        <Card />
-
+                  : "No Such Data"}
+              </div>
+            );
+          })
+        ) : (
+          <div>"""""""""""""""""""""""</div>
+        )}
       </div>
-      <div> <Footer /></div>
-
-    </div >
+      <div>
+        {" "}
+        <Footer />
+      </div>
+    </div>
   );
 }
